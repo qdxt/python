3bdbe6e3c34a5c3167b5cf86ef086d9385b9bf32
yinwei <980165003@qq.com>
2017-12-08 15:18:53 +0800

    number

---
+++ test/number.py
@@ -0,0 +1,29 @@
+#number
+#python数字数据类型用于储存数值
+#数据类型是不允许改变的，这就意味着如果改变数字数据类型得值，将重新分配内存空间。
+var1 = 10
+var2 = 20
+#你也可以使用del语句删除一些数字对象得引用
+#语法：
+#del var1[,var2[,var3[....,varN]]]
+
+#您可以通过使用del语句删除单个或多个对象的引用：
+
+# del var1
+# del var_a, var_b
+
+#python支持三种不同的数据类型
+
+#整数(Int)
+
+#浮点型(float)
+
+#复数(complex)
+
+#我们也可以使用十六进制和八进制来代表整数
+number1 = 0xA0F # 十六进制
+print ('number1',number1)
+
+number2 = 0o37 # 八进制
+print('number2',number2)
+
