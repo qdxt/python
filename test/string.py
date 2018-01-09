220acb77d090aad06d4851f48311b2cede28d523
yinwei <980165003@qq.com>
2018-01-09 13:19:52 +0800

    增加练习文件夹

--- test/string.py
+++ test/string.py
@@ -74,7 +74,7 @@ print(R'\n')
 # 1
 # capitalize()
 # 将字符串的第一个字符转换为大写
-# 2	
+# 2
 # center(width, fillchar)
 #
 # 返回一个指定的宽度 width 居中的字符串，fillchar 为填充的字符，默认为空格。
---
+++ test/test.py
@@ -0,0 +1,21 @@
+# Fibonacci series: 斐波纳契数列
+# 两个元素的总和确定了下一个数
+
+# money = 2000
+# def addmoney():
+#     global money
+#     money = money+1
+#     print(money)
+#
+# addmoney()
+#
+# print(money)
+# 打开文件
+# fo = open("sys.py")
+# print(fo.name)
+# # 关闭文件
+# fo.close()
+# 创建文件夹
+# import os
+# os.mkdir("lesson")
+
